b81afbfd5e2a15f08929e784294a32842fe4dd27
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-22 23:53:14 +1200

    Genre | Data insertion

---
+++ GenreInserts.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO Genre (GenreID, Name) VALUES
+(1, 'Rock'),
+(2, 'Pop'),
+(3, 'Hip Hop'),
+(4, 'Jazz'),
+(5, 'Classical'),
+(6, 'Electronic'),
+(7, 'Country'),
+(8, 'Reggae'),
+(9, 'Metal'),
+(10, 'Indie');
