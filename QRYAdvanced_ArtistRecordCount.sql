352db9f1399266ebca1b480480af958d12e939a0
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-24 23:58:28 +1200

    Artist record count query (advanced query 3)
    Returns a query that displays artists and their total records, listed in ascending order (A-Z)

---
+++ QRYAdvanced_ArtistRecordCount.sql
@@ -0,0 +1,4 @@
+﻿SELECT Artist.ArtistName, COUNT(Record.RecordID) AS Records
+FROM Artist INNER JOIN Record ON Artist.ArtistID = Record.ArtistID
+GROUP BY Artist.ArtistName
+ORDER BY Artist.ArtistName ASC
