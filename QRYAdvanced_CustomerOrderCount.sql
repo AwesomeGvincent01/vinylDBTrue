d19594def4ed96916344998f048c22db842de665
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-24 23:07:21 +1200

    Customer order count query (advanced query 1)
    For the advanced query, I decided I would display customers and their total orders.
    There were plenty of errors initially. One being the for "incorrect syntax near '.'", which
    I was confused about, as it was underscoring the full stop in red. But then I realized
    since I was doing an aggregate count on the Order.CustomerID selection, I had to
    enclose the "Order.CustomerID" part in brackets, but I was still getting an error.
    But I realized it way quicker this time. I had forgotten to enclose the "Order" for
    referencing the order table in square brackets, as Order is a reserved sql keyword.
    I already knew this for the longest time, but had forgotten today as I was a bit tired, so I had a bit of what you would call a "brain fart" or blunder, whatever u wanna call  it. Then, I executed the code, but I saw in the results "invalid column name 'Customer'.. then I realized. oh wow. I just did group by a table. what is going on with me today??
    so I changed it to be group by first name. then, another error in the results.. "Invalid column name 'COUNT'. Then, I remembered the AS or alias keyword. I decided to temporarily rename the aggregate count select (CustomerID from orders) to "TotalOrders", and tried doing order by that name instead, and what do you know... it worked.
    
    initial:
    
    SELECT Customer.FirstName, Customer.LastName, COUNT [Order].CustomerID
    FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
    Group by Customer
    ORDER BY COUNT DESC
    
    end:
    
    SELECT Customer.FirstName, COUNT ([Order].CustomerID) AS TotalOrders
    FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
    Group by FirstName
    ORDER BY COUNT DESC
    
    Oh wait, and for the end result I got rid of the last name column for some reason, because I thought u couldn't group by two columns... anyways, I added it back, and grouped by both columns. I also added table specifications to the columns I grouped by (.Customer) just because I heard It's good practice.
    
    true end result:
    
    SELECT Customer.FirstName, Customer.LastName, COUNT ([Order].CustomerID) AS TotalOrders
    FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
    Group by Customer.FirstName, Customer.LastName
    ORDER BY TotalOrders DESC

---
+++ QRYAdvanced_CustomerOrderCount.sql
@@ -0,0 +1,5 @@
+﻿SELECT Customer.FirstName, Customer.LastName, COUNT ([Order].CustomerID) AS TotalOrders
+FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
+Group by Customer.FirstName, Customer.LastName
+ORDER BY TotalOrders DESC
+
