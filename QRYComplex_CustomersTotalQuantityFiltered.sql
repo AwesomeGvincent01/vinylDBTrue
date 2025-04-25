9b824719874e0a188da09adf02338fe98a5939e8
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-25 23:27:34 +1200

    Customers total quantity filtered query (Complex query 2)
    Prompts the user to input a number, and then returns a query that displays all customers who have a quantity greater than the number that was inputted.

---
+++ QRYComplex_CustomersTotalQuantityFiltered.sql
@@ -0,0 +1,7 @@
+﻿DECLARE @Quantity INT = '1'
+SELECT FirstName, LastName, SUM(Quantity) AS TotalQuantity
+FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
+INNER JOIN OrderDetail ON OrderDetail.OrderID = [Order].OrderID
+GROUP BY FirstName, LastName
+HAVING SUM(Quantity) > @Quantity
+
