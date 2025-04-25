b7ec9fd7909e9a8747f7bb7131236279942298c6
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-25 23:58:01 +1200

    Customer/artist total quantity query (advanced query 3)
    Returns a query that displays total quantity of records purchased by each customer, grouped by artist

---
+++ QRYComplex_ArtistTotalQuantity_.sql
@@ -0,0 +1,6 @@
+﻿SELECT FirstName, LastName, ArtistName, SUM(Quantity) AS TotalQuantity
+FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
+INNER JOIN OrderDetail ON [Order].OrderID = OrderDetail.OrderID
+INNER JOIN Record ON OrderDetail.RecordID = Record.RecordID
+INNER JOIN Artist ON Record.ArtistID = Artist.ArtistID
+GROUP BY FirstName, LastName, ArtistName
