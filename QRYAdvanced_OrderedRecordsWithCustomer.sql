1c39d90279562ca17f4de90d1d7d15869e8c4d60
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-25 22:25:33 +1200

    Ordered records w/ customer query (advanced query 4)
    Returns a query that lists records from orders with customer names, order dates and quantity using joins across 4 tables

---
+++ QRYAdvanced_OrderedRecordsWithCustomer.sql
@@ -0,0 +1,4 @@
+﻿SELECT Customer.FirstName, Customer.Lastname, Record.Title, [Order].OrderDate, Orderdetail.Quantity
+FROM Customer INNER JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
+INNER JOIN OrderDetail ON [Order].OrderID = OrderDetail.OrderID
+INNER JOIN Record ON OrderDetail.RecordID = Record.RecordID
