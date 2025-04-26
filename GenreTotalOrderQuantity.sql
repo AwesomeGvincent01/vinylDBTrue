bb8e6d78302a800b60cba5e990a00b54d3efdfb1
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-26 22:38:57 +1200

    Total order quantity for each genre query (complex query 5)
    Returns a query that displays the total quantity of records ordered per genre

---
+++ GenreTotalOrderQuantity.sql
@@ -0,0 +1,7 @@
+﻿SELECT Genre.[Name], SUM(Quantity) AS TotalQuantity
+FROM Record INNER JOIN Genre ON Record.GenreID = Genre.GenreID
+INNER JOIN OrderDetail ON Record.RecordID = OrderDetail.RecordID
+GROUP BY Genre.[Name]
+ORDER BY TotalQuantity DESC
+
+
