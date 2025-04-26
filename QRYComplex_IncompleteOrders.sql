5d7f099101b9c7883383e2968dd53ecd1047db78
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-04-26 21:52:18 +1200

    Incomplete orders query
    Returns a query that displays all customers who have not completed an order (missing/no matching entry in OrderDetail tablee)

---
+++ QRYComplex_IncompleteOrders.sql
@@ -0,0 +1,6 @@
+﻿SELECT FirstName, LastName, SUM(Quantity) AS Total
+FROM Customer LEFT JOIN [Order] ON Customer.CustomerID = [Order].CustomerID
+LEFT JOIN OrderDetail ON [Order].OrderID = OrderDetail.OrderID
+GROUP BY Customer.FirstName, Customer.LastName
+HAVING SUM(Quantity) IS NULL
+
